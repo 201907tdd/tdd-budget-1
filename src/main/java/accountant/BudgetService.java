138d76e037d73db7cf8216d4f5554bda8bb7e473
Joey Chen <jay.hatelove@gmail.com>
2019-07-28 00:32:19 +0800

    remove query single date, because of included in query same month

--- src/main/java/accountant/BudgetService.java
+++ src/main/java/accountant/BudgetService.java
@@ -19,15 +19,15 @@ public class BudgetService {
             return 0;
         }
 
-        if (start.isEqual(end)) {
-            Optional<Budget> budget = getBudget(start);
-
-            if (budget.isPresent()) {
-                return budget.get().dailyAmount();
-            }
-
-            return 0;
-        }
+//        if (start.isEqual(end)) {
+//            Optional<Budget> budget = getBudget(start);
+//
+//            if (budget.isPresent()) {
+//                return budget.get().dailyAmount();
+//            }
+//
+//            return 0;
+//        }
 
         if (diffMonth(start, end) == 0) {
 
