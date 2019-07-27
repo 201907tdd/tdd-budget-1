175b0c34889480ae895a640d2ff607956fa0b871
Joey Chen <jay.hatelove@gmail.com>
2019-07-28 01:49:38 +0800

    clean up BudgetService

--- src/main/java/accountant/BudgetService.java
+++ src/main/java/accountant/BudgetService.java
@@ -19,30 +19,12 @@ public class BudgetService {
             return 0;
         }
 
-        double totalAmount = 0;
-
         Period period = new Period(start, end);
-        LocalDate currentDate = start;
-//        while (currentDate.isBefore(YearMonth.from(end).atDay(1).plusMonths(1))) {
-//
-//            Optional<Budget> currentBudget = getBudget(currentDate);
-//            if (currentBudget.isPresent()) {
-//                Budget budget = currentBudget.get();
-//
+        double totalAmount = 0;
         for (Budget budget : budgetRepo.getAll()) {
 
             totalAmount += budget.getOverlappingAmount(period);
         }
-//            }
-//            currentDate = currentDate.plusMonths(1);
-//        }
         return totalAmount;
-    }
-
-    private Optional<Budget> getBudget(LocalDate currentDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-        return budgetRepo.getAll()
-                .stream()
-                .filter(b -> b.getYearMonth().equals(currentDate.format(formatter))).findFirst();
     }
 }
